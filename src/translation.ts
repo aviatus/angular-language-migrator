592b290bf22903847ab85fb7b4c04fce502fb775
ahmet <aenes45@gmail.com>
2022-01-23 20:14:43 +0300

    Fix functions order in translation

--- src/translation.ts
+++ src/translation.ts
@@ -19,8 +19,45 @@ export function createTranslationJSON() {
     return translateMap;
 }
 
-export function getKey(map: Map<string, string> | [string, string][], val: string): string {
-    return [...map].find(([, value]) => val === value)?.[0] as string;
+export function createTextMap(text: string[], fileName: string) {
+    return [...new Map(text.map((t: string, index: number) => {
+        const key = fileName + '_' + base.substring(0, base.length - index.toString().length) + index.toString();
+        return [key, t];
+    }))];
+}
+
+export function setTranslates(moduleName: string, moduleTranslates: Map<string, string>) {
+    const currentTranslates = translates.get(moduleName);
+    let keys: Map<string, string>;
+    if (currentTranslates?.size) {
+        keys = mergedMaps(currentTranslates, moduleTranslates);
+    } else {
+        keys = moduleTranslates;
+    }
+
+    translates.set(moduleName, keys);
+}
+
+export function travelDOMNodes(node: Node) {
+    const texts = [];
+    do {
+        if (node?.textContent && node.nodeType !== 8) {
+            const text = node?.textContent.replace(/\n/g, '');
+            const isTextFitForTranslation = checkTextFromNodeFitForTranslation(text);
+
+            if (isTextFitForTranslation) {
+                const cleanedText = text.replace(/\s+/g, ' ');
+                texts.push(cleanedText);
+            }
+        }
+
+        node = node.firstChild as Node || node.nextSibling as Node || function () {
+            while ((node = node.parentNode as Node) && !node.nextSibling);
+            return node ? node.nextSibling : null;
+        }();
+    } while (node);
+
+    return [...new Set(texts)];
 }
 
 export function getUppercase(text: string): string {
@@ -32,11 +69,8 @@ export function getModuleName(filePath: string) {
     return moduleIndex > 0 ? getUppercase(filePath.split('\\')[moduleIndex + 1]) : '';
 }
 
-export function createTextMap(text: string[], fileName: string) {
-    return [...new Map(text.map((t: string, index: number) => {
-        const key = fileName + '_' + base.substring(0, base.length - index.toString().length) + index.toString();
-        return [key, t];
-    }))];
+export function removeParentNodeTranslateDuplications(texts: string[]) {
+    return texts.filter((translate: string) => !texts.some((t: string) => t !== translate && translate.includes(t)));
 }
 
 export function replaceHtmlTexts(text: string[], textMap: [string, string][], data: string, filePath: string, moduleName: string): Promise<void> {
@@ -54,19 +88,7 @@ export function replaceHtmlTexts(text: string[], textMap: [string, string][], da
     });
 }
 
-export function setTranslates(moduleName: string, moduleTranslates: Map<string, string>) {
-    const currentTranslates = translates.get(moduleName);
-    let keys: Map<string, string>;
-    if (currentTranslates?.size) {
-        keys = mergedMaps(currentTranslates, moduleTranslates);
-    } else {
-        keys = moduleTranslates;
-    }
-
-    translates.set(moduleName, keys);
-}
-
-export function mergedMaps(...maps: Map<string, string>[]): Map<string, string> {
+function mergedMaps(...maps: Map<string, string>[]): Map<string, string> {
     const dataMap = new Map<string, string>([]);
     for (const map of maps) {
         for (const [key, value] of map) {
@@ -77,30 +99,15 @@ export function mergedMaps(...maps: Map<string, string>[]): Map<string, string> 
     return dataMap;
 }
 
-export function removeParentNodeTranslateDuplications(texts: string[]) {
-    return texts.filter((translate: string) => !texts.some((t: string) => t !== translate && translate.includes(t)));
+function getKey(map: Map<string, string> | [string, string][], val: string): string {
+    return [...map].find(([, value]) => val === value)?.[0] as string;
 }
 
-export function travelDOMNodes(node: Node) {
-    const texts = [];
-    do {
-        if (node?.textContent && node.nodeType !== 8) {
-            const text = node?.textContent.replace(/\n/g, '');
-            const trimmedText = text.trim();
-            const isTextFitForTranslation = trimmedText.length > 0 && punctiations.every((p) => trimmedText !== p) &&
-                bannedChars.every(char => !text.includes(char)) && isNaN(+text);
+function checkTextFromNodeFitForTranslation(text: string): boolean {
+    const trimmedText = text.trim();
+    const isNotPunctiation = punctiations.every((p) => trimmedText !== p);
+    const isNotContainBannedChars = bannedChars.every(char => !text.includes(char));
+    const isNotNumber = isNaN(+text);
 
-            if (isTextFitForTranslation) {
-                const cleanedText = text.replace(/\s+/g, ' ');
-                texts.push(cleanedText);
-            }
-        }
-
-        node = node.firstChild as Node || node.nextSibling as Node || function () {
-            while ((node = node.parentNode as Node) && !node.nextSibling);
-            return node ? node.nextSibling : null;
-        }();
-    } while (node);
-
-    return [...new Set(texts)];
+    return trimmedText.length > 0 && isNotPunctiation && isNotContainBannedChars && isNotNumber;
 }
